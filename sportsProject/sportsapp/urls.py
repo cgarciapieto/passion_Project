48f6d63ed53b426c2d72a330e5759615ca5f10bf
carlos <cgarciapieto@gmail.com>
2019-05-29 12:35:40 -0500

    12pm_push

--- sportsProject/sportsapp/urls.py
+++ sportsProject/sportsapp/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('createPost/', views.createPost, name='createPost'),
+    path('viewPost/', views.viewPost, name='viewPost'),
+    path('postList/', views.postList, name='postList'),
+    path('editPost/<int:post_id>/', views.editPost, name='editPost'),
+    path('createUser/', views.createUser, name='createUser'),
+    path('postDetails/', views.postDetails, name='postDetails'),
+    path('deletePost/<int:post_id>/', views.deletePost, name='deletePost'),
+    # path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
+
 ]
--- sportsProject/sportsapp/views.py
+++ sportsProject/sportsapp/views.py
@@ -1,8 +1,49 @@
 from django.shortcuts import render
-
-
 from django.http import HttpResponse
+from .models import UserModel, PostModel
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, get_object_or_404
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # if request.user.is_authenticated:
+    #     print(request.user)
+    #
+    #     authUser = UserModel.objects.get(username=request.user)
+    #
+    #     allEntries = PostModel.objects.filter(foreignkeyToUserModel=authUser)
+    # else:
+    #
+    #     allEntries =''
+    #     context = {'allEnttries': allEntries}
+
+
+    # return render('sportsapp/index.html', context)
+    return HttpResponse("Index")
+
+def createPost(request):
+
+    return HttpResponse("Create Post")
+
+
+def viewPost(request):
+    return HttpResponse("view Post")
+
+
+def postList():
+    return HttpResponse("post list")
+
+
+def createUser(request):
+    return HttpResponse("Create user")
+
+def postDetails(request):
+    return HttpResponse(" Details")
+
+
+def editPost(request):
+    return HttpResponse("edit Post")
+
+
+def deletePost(request):
+    return HttpResponse("delete Post")
